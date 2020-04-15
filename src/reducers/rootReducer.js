ecad08c41d52b2b715306abd42ccb61ea9afb484
Sam Markham <sam.e.markham@gmail.com>
2020-04-15 16:56:00 -0700

    Adds sample rootReducer code

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+// import manageRestaurants from './manageRestaurant'
+// import manageReviews from './manageReview'
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+  // restaurants: manageRestaurants,
+  // reviews: manageReviews
+})
+
+export default rootReducer;
