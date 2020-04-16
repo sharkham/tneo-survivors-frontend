da680f6ecdd13e5b56298fa849e5b6a37cc0c8a3
Sam Markham <sam.e.markham@gmail.com>
2020-04-16 12:21:34 -0700

    Builds mock userinfo component

---
+++ src/components/UserInfo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class UserInfo extends Component {
+  render() {
+    return (
+      <div>
+        Title: Novel Title
+        Summary: Novel Summary
+        Plan of Attack: Novel Plan
+        Goal: 50000
+        Word Count: 10000
+      </div>
+    );
+  }
+}
+
+export default UserInfo;
