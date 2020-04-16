5ab051d93c987e578a452a164e15d7a28b1cc948
Sam Markham <sam.e.markham@gmail.com>
2020-04-16 12:42:41 -0700

    Builds out update word count form component

--- src/components/UpdateWordCountForm.js
+++ src/components/UpdateWordCountForm.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const UpdateWordCountForm = () => {
-  return (
-    <div>
-      Update Word Count Form
-    </div>
-  );
+class UpdateWordCountForm extends Component {
+  state = {
+    wordcount: ""
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    //the part where wordcount gets submitted somewhere!
+    console.log(this.state.wordcount)
+    this.setState({
+      wordcount: ""
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Update Word Count (please enter the number of words you wish to add):</label>
+        <input id="wordcount" type="number" min="1" name="wordcount" onChange={this.handleChange} value={this.state.wordcount}/>
+        <input type="submit"/>
+      </form>
+    );
+  }
 }
 
 export default UpdateWordCountForm;
+
