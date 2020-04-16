744774eb45e9a526086510bf2047a0040d400333
Sam Markham <sam.e.markham@gmail.com>
2020-04-16 12:20:04 -0700

    Builds out user page and adds UserInfo and UserBadgesContainer components

---
+++ src/components/UserPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import UserInfo from './UserInfo'
+import UserBadgesContainer from
+
+class UserPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Username</h1>
+        <UserInfo />
+        <UserBadgesContainer />
+      </div>
+    );
+  }
+}
+
+export default UserPage;
