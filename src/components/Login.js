9cc18487919a24d06119cc8e1dd7ccae3bdfea6d
Sam Markham <sam.e.markham@gmail.com>
2020-04-16 15:26:48 -0700

    Adds form to Login component

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <form onSubmit={}>
+      <input name="username" type="text" onChange={} value={}/>
+      <input name="password" type="password" onChange={} value={}/>
+      <input type="submit" value="Log In"/>
+    </form>
+  );
+}
+
+export default Login;
+
+
