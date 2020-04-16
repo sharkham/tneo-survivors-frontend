cc0f31ba61db4614fc430cde80d84279279a412b
Sam Markham <sam.e.markham@gmail.com>
2020-04-16 14:05:20 -0700

    Fills in rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,14 @@
 // import manageRestaurants from './manageRestaurant'
 // import manageReviews from './manageReview'
 import { combineReducers } from 'redux'
+import users from './users'
+import novels from './novels'
+import badges from './badges'
 
 const rootReducer = combineReducers({
+  users,
+  novels,
+  badges
   // restaurants: manageRestaurants,
   // reviews: manageReviews
 })
