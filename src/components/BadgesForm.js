f08a5dca33887c1483b75c47bcb39a82aff67cec
Sam Markham <sam.e.markham@gmail.com>
2020-04-16 13:02:25 -0700

    Builds out badges form

--- src/components/BadgesForm.js
+++ src/components/BadgesForm.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const BadgesForm = () => {
+const BadgesForm = (props) => {
+  //render the badges in the form from badgetype props passed down
   return (
-    <div>
-      Badges Form
-    </div>
+    <form>
+      <select className="badgeselect">
+        <option value="">--Please choose a badge--</option>
+        {props.badgetypes.forEach(badgetype => {
+          //make new Option for each badgetype
+          return <option value={badgetype.id}>{badgetype.name} - {badgetype.description}</option>
+        })}
+      </select>
+    </form>
   );
 }
 
