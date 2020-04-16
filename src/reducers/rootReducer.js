9c31927194f7ba037cb13ceb2732d51c1a853508
Sam Markham <sam.e.markham@gmail.com>
2020-04-16 15:20:17 -0700

    Adds loginForm actions file

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -5,10 +5,12 @@ import users from './users'
 import novels from './novels'
 import badges from './badges'
 import currentUser from './currentUser'
+import loginForm from './loginForm'
 
 const rootReducer = combineReducers({
   users,
   currentUser,
+  loginForm,
   novels,
   badges
   // restaurants: manageRestaurants,
