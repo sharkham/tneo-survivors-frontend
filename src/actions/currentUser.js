6aa6d3a573b13803281ae54070ec83e647a44315
Sam Markham <sam.e.markham@gmail.com>
2020-04-16 15:04:04 -0700

    builds out fetch method in actions/currentUser

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -12,6 +12,14 @@ export const setCurrentUser = user => {
 //asynchronous action creators
 export const login = credentials => {
   return dispatch => {
-    return fetch()
+    return fetch("http://localhost:3000/api/v1/login", {
+      //still creating something--creating a session, that's why it's a POST
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(credentials)
+      //body: JSON.stringify({username: "marysue1", password: "password"})
+    })
   }
 }
