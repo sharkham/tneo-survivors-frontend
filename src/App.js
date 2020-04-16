7fefb2645bdaa48d9f6e83b8e3ec11ee109bc450
Sam Markham <sam.e.markham@gmail.com>
2020-04-15 17:45:16 -0700

    Adds home route to App and adds NavBar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <Router>
+        <div>
+          <NavBar />
+          {/* This will eventually lead to the view page */}
+          <Route exact path="/" render={() => <div>Home</div>} />
+          {/* <Route path='/movies' render={routerProps => <MoviesPage {...routerProps} movies={this.state.movies}/>} /> */}
+        </div>
+      </Router>
     );
   }
 }
