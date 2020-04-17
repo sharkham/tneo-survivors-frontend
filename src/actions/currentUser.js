7b3fba4976f9743c390a4cd40b4335462fc09d74
Sam Markham <sam.e.markham@gmail.com>
2020-04-17 12:36:53 -0700

    Adds async logout action in actions/currentUser

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -34,6 +34,15 @@ export const login = credentials => {
   }
 }
 
+export const logout = () => {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/logout", {
+      credentials: "include",
+      method: "DELETE"
+    })
+  }
+}
+
 export const getCurrentUser = () => {
   return dispatch => {
     return fetch("http://localhost:3000/api/v1/get_current_user", {
