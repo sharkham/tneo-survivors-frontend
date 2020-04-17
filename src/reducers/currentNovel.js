169a36c3d70f1af2c332378e389d3405d358a6cb
Sam Markham <sam.e.markham@gmail.com>
2020-04-17 14:10:55 -0700

    Adds CLEAR_CURRENT_USER case to currentNovel reducer and sets currentNovel to null when there is no logged in user

--- src/reducers/currentNovel.js
+++ src/reducers/currentNovel.js
@@ -2,6 +2,8 @@ export default function currentNovel(state = null, action) {
   switch (action.type) {
     case 'SET_CURRENT_NOVEL':
       return action.novel
+    case 'CLEAR_CURRENT_USER':
+      return null
     default:
       return state
   }
