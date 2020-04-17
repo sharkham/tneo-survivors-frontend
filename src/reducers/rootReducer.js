3fa35dd420f18f1ec2f8bd40013c681e2d9f152b
Sam Markham <sam.e.markham@gmail.com>
2020-04-16 22:09:00 -0700

    Removes loginForm from rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -5,12 +5,10 @@ import users from './users'
 import novels from './novels'
 import badges from './badges'
 import currentUser from './currentUser'
-import loginForm from './loginForm'
 
 const rootReducer = combineReducers({
   users,
   currentUser,
-  loginForm,
   novels,
   badges
   // restaurants: manageRestaurants,
