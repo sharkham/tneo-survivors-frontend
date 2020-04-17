a86aaf4dd643900eedd6ae1d15a7b6d9c60afc8a
Sam Markham <sam.e.markham@gmail.com>
2020-04-17 15:08:22 -0700

    Changes URL for patchWordCount fetch again

--- src/actions/currentNovel.js
+++ src/actions/currentNovel.js
@@ -48,7 +48,7 @@ export const getNovel = user => {
 export const patchWordCount = novel => {
   console.log("patching word count")
   return dispatch => {
-    return fetch(`http://localhost:3000/api/v1/novels/${novel.id}/update`, {
+    return fetch(`http://localhost:3000/api/v1/novels/${novel.id}`, {
       credentials: "include",
       method: "PATCH",
       headers: {
