dc6346f0bdc069dabbe85f5c8556e92fe29dfddf
Sam Markham <sam.e.markham@gmail.com>
2020-04-16 22:00:59 -0700

    Adds code to UserInfo to attempt to display current user username

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import UpdateWordCountForm from './UpdateWordCountForm'
 import BasicInfoDisplay from './BasicInfoDisplay'
+import { connect } from 'react-redux';
 
 class UserInfo extends Component {
   //this component is going to access store to get and set user info
+  // componentDidMount = () => {
+  //   console.log(this.props.user)
+  // }
 
+  // renderUsername = () => {
+  //   if (!!this.props.user.username) {
+  //     return this.props.user.username
+  //   }
+  // }
 
   render() {
     return (
       <div>
-        <h1>Username (from store)</h1>
+        <h1>User</h1>
         <BasicInfoDisplay />
         <UpdateWordCountForm />
       </div>
@@ -17,4 +26,10 @@ class UserInfo extends Component {
   }
 }
 
-export default UserInfo;
+const mapStateToProps = state => {
+  return {
+    user: state.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(UserInfo);
