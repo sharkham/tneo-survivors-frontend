ebdf57395776e486f36492aee344bf3e34d46c20
Sam Markham <sam.e.markham@gmail.com>
2020-04-17 14:31:25 -0700

    Edits basicinfodisplay component so it displays info from the novel prop passed in

--- src/components/BasicInfoDisplay.js
+++ src/components/BasicInfoDisplay.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const BasicInfoDisplay = () => {
+const BasicInfoDisplay = (props) => {
   return (
     <div>
-        Title: Novel Title
-        Summary: Novel Summary
-        Plan of Attack: Novel Plan
-        Goal: 50000
-        Word Count: 10000
+        <p>Title: {props.novel.title}</p>
+        <p>Summary: {props.novel.summary}</p>
+        <p>Plan of Attack: {props.novel.plan}</p>
+        <p>Goal: {props.novel.goal}</p>
+        <p>Word Count: {props.novel.wordcount}</p>
     </div>
   );
 }
