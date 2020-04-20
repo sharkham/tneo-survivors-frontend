45302276148a92b535504b16f81e168f75ae4b64
Sam Markham <sam.e.markham@gmail.com>
2020-04-20 11:44:09 -0700

    Builds out NovelForm component and adds list of plan types for select form

--- src/components/NovelForm.js
+++ src/components/NovelForm.js
@@ -8,6 +8,18 @@ class NovelForm extends Component {
     goal: ""
   }
 
+  planTypes = () => {
+    return [
+      "November Warrior",
+      "Slow and Steady",
+      "Revise Revise",
+      "Hi, I'm in School",
+      "A Little of Everything",
+      "Condensed Timeline",
+      "My Way or the Highway"
+    ]
+  }
+
   handleChange = (event) => {
     const { name, value } = event.target
     this.setState({
@@ -34,6 +46,13 @@ class NovelForm extends Component {
       <form onSubmit={this.handleSubmit}>
         <input id="title" type="text" name="title" onChange={this.handleChange} value={this.state.title}/>
         <input id="summary" type="textarea" name="summary" onChange={this.handleChange} value={this.state.summary}/>
+        <select id="plan" name="plan" onChange={this.handleChange} value={this.state.plan}>
+          <option value="">--Please choose a plan--</option>
+          {this.planTypes().map(planType => {
+            //make new Option for each planType
+            return <option key={planType} value={planType}>{planType}</option>
+          })}
+        </select>
         {/* <input id="plan" type="text" min="1" name="plan" onChange={this.handleChange} value={this.state.plan}/> */}
         <input id="goal" type="number" min="1" name="goal" onChange={this.handleChange} value={this.state.goal}/>
         <input type="submit"/>
