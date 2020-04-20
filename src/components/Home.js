0dc3b2d2c2d625169baa120b6504f3a0b580cbaa
Sam Markham <sam.e.markham@gmail.com>
2020-04-20 10:11:59 -0700

    Adds sign up and login links to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Login from './Login';
+import Signup from './Signup';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-
+      <h4>Welcome, please <Link to="/signup">sign up</Link> or <Link to="/login">log in</Link>.</h4>
     </div>
   );
 }
