34db24fdfe7a26f4af6adc79cf8690e57902bd8a
Sam Markham <sam.e.markham@gmail.com>
2020-04-20 10:03:10 -0700

    Adds Home and AllNovelsIndex components

---
+++ src/components/AllNovelsIndex.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AllNovelsIndex extends Component {
+  render() {
+    return (
+      <div>
+        Will display everyone else's novels
+      </div>
+    );
+  }
+}
+
+export default AllNovelsIndex;
