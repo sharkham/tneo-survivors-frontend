0dc0bc6b9ea04ab37cc7d71570886bd6a27ad947
Sam Markham <sam.e.markham@gmail.com>
2020-04-21 11:36:35 -0700

    Adds UserInfo component and moves name and username display there from UserPage component

---
+++ src/components/UserInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Username = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.username}</h2>
+    </div>
+  );
+}
+
+export default Username;
--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NovelInfo from './NovelInfo';
 import NovelBadgesContainer from './NovelBadgesContainer';
+import UserInfo from './UserInfo'
 import { connect } from 'react-redux';
 
 class UserPage extends Component {
@@ -14,18 +15,20 @@ class UserPage extends Component {
   //   }
   // }
 
-  renderUsername = () => {
-    if (!!this.props.user) {
-      return this.props.user.name
-    }
-  }
+  // renderUsername = () => {
+  //   if (!!this.props.user) {
+  //     return this.props.user.name
+  //   }
+  // }
 
   render() {
     return (
       <div>
         {/* Add conditional rendering--if User has no novel User is prompted to create novel */}
         {/* {this.createNovelPrompt()} */}
-        <h1>{this.renderUsername()}</h1>
+        {/* Maybe turn this into a stateless functional component where username is passed down as props */}
+        {/* <h1>{this.renderUsername()}</h1> */}
+        <UserInfo name={this.props.user.name} username={this.props.user.username}/>
         <NovelInfo />
         <NovelBadgesContainer />
       </div>
