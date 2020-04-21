d2d7b15b05f49d08baaa142fb3f405b7449b5a31
Sam Markham <sam.e.markham@gmail.com>
2020-04-21 14:50:45 -0700

    fixes add novel case in novels reducer

--- src/reducers/novels.js
+++ src/reducers/novels.js
@@ -1,9 +1,8 @@
 export default function novels(state = [], action) {
   switch (action.type) {
     case 'ADD_NOVEL':
-      return {
-        ...state
-      }
+      return
+        action.novel
     default:
       return state
   }
