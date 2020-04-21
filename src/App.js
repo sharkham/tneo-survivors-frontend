3a0da32d725e0c82679295bb88c4b51a4c3533ee
Sam Markham <sam.e.markham@gmail.com>
2020-04-21 11:56:56 -0700

    Moves BrowserRouter wrapper from App to index

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {
-  BrowserRouter as Router,
   Route,
   Redirect
 } from 'react-router-dom';
@@ -28,7 +27,6 @@ class App extends Component {
   render() {
     const loggedIn = this.props.loggedIn
     return (
-      <Router>
         <div>
           <NavBar />
           {/* This will eventually lead to the view page */}
@@ -42,7 +40,6 @@ class App extends Component {
           <Route path="/profile">{loggedIn ? <UserPage/> : <Redirect to="/"/>}</Route>
           {/* <Route path='/movies' render={routerProps => <MoviesPage {...routerProps} movies={this.state.movies}/>} /> */}
         </div>
-      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -13,9 +16,11 @@ const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
