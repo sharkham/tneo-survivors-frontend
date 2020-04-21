4e676215336abedc024c7b8906d820817d29a89a
Sam Markham <sam.e.markham@gmail.com>
2020-04-21 14:42:40 -0700

    Further cleans up NovelPage

--- src/components/NovelPage.js
+++ src/components/NovelPage.js
@@ -10,16 +10,14 @@ import { Route, Switch, Redirect } from "react-router-dom";
 class NovelPage extends Component {
 
   render() {
+    const hasNovel = this.props.hasNovel
     return (
       <div>
-        {/* Add conditional rendering--if User has no novel User is prompted to create novel */}
-        {/* {this.createNovelPrompt()} */}
-        {/* <UserInfo name={this.props.user.name} username={this.props.user.username}/> */}
         <Switch>
-          <Route path="/novel/edit">{this.props.hasNovel ? <div><EditNovel novel={this.props.novel}/><NovelBadgesContainer/></div> : <Redirect to="/novel/create"/>}</Route>
-          <Route path="/novel/create">{this.props.hasNovel ? <Redirect to="/novel"/> : <CreateNovel/>}</Route>
+          <Route path="/novel/edit">{hasNovel ? <div><EditNovel novel={this.props.novel}/><NovelBadgesContainer/></div> : <Redirect to="/novel/create"/>}</Route>
+          <Route path="/novel/create">{hasNovel ? <Redirect to="/novel"/> : <CreateNovel/>}</Route>
 
-          <Route path="/novel">{this.props.hasNovel ? <div><NovelInfo/><NovelBadgesContainer/></div> : <Redirect to="/novel/create"/>}</Route>
+          <Route path="/novel">{hasNovel ? <div><NovelInfo/><NovelBadgesContainer/></div> : <Redirect to="/novel/create"/>}</Route>
         </Switch>
       </div>
     );
