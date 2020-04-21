895a96deed098d8fe5699d1bded8f99f6ca86191
Sam Markham <sam.e.markham@gmail.com>
2020-04-21 13:05:10 -0700

    Removes unnecessary props from NovelInfo

--- src/components/NovelInfo.js
+++ src/components/NovelInfo.js
@@ -7,40 +7,13 @@ import { getNovel, patchWordCount } from '../actions/currentNovel';
 
 
 class NovelInfo extends Component {
-  //this component is going to access store to get and set novel info
-  // componentDidUpdate() {
-  //   if (!!this.props.user && !this.props.novel) {
-  //     console.log("attempting")
-  //     this.props.getNovel(this.props.user)
-  //   }
-  // }
 
-  //this component should only display if user has a novel, the "create a novel" flicker is frustrating
-  //and create a novel should be a different component
-  // novelInfoOrPrompt = () => {
-  //   // if (!!this.props.novel) {
-  //     return (
-  //       <div>
-  //         <BasicInfoDisplay novel={this.props.novel}/>
-  //         <UpdateWordCountForm novel={this.props.novel} wordcount={this.props.novel.wordcount} patchWordCount={this.props.patchWordCount}/>
-  //       </div>
-  //     );
-  //   // } else {
-  //   //   return (
-  //   //     <CreateNovel/>
-  //   //   )
-  //   // }
-
-  // }
 
   render() {
     return (
       <div>
         <BasicInfoDisplay novel={this.props.novel}/>
         <UpdateWordCountForm novel={this.props.novel} wordcount={this.props.novel.wordcount} patchWordCount={this.props.patchWordCount}/>
-        {/* {this.novelInfoOrPrompt()} */}
-        {/* <BasicInfoDisplay />
-        <UpdateWordCountForm /> */}
       </div>
     );
   }
@@ -48,7 +21,7 @@ class NovelInfo extends Component {
 
 const mapStateToProps = state => {
   return {
-    user: state.currentUser,
+    // user: state.currentUser,
     novel: state.currentNovel
   }
 }
