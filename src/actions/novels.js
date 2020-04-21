bb2e49b5d377297e48192a1f992812f669bf3406
Sam Markham <sam.e.markham@gmail.com>
2020-04-21 15:23:26 -0700

    Adds novels action file and synchronous and asynchronous actions for adding a novel

---
+++ src/actions/novels.js
@@ -0,0 +1,34 @@
+const baseURL = "http://localhost:3000/api/v1/novels"
+
+//synchronous action creators
+
+export const addNovel = novel => {
+  return {
+    type: "ADD_NOVEL",
+    novel
+  }
+}
+
+
+//asynchronous action creators
+export const createNovel = (novel) => {
+  return dispatch => {
+    return fetch(`${baseURL}`, {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(novel)
+    })
+    .then(res => res.json())
+    .then(novel => {
+      if (novel.error) {
+        alert(novel.error)
+      } else {
+        dispatch(addNovel(novel))
+      }
+    })
+    .catch(console.log)
+  }
+}
