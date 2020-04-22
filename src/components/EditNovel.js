7ec8214e7b12c639e79c693e731123299c920d7f
Sam Markham <sam.e.markham@gmail.com>
2020-04-21 19:43:37 -0700

    Fixes EditNovel to work on its own without NovelForm

--- src/components/EditNovel.js
+++ src/components/EditNovel.js
@@ -1,35 +1,101 @@
-import React from 'react';
-import NovelForm from './NovelForm';
+import React, { Component } from 'react';
+// import NovelForm from './NovelForm';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { patchNovel } from '../actions/novels';
 
 
-const EditNovel = ({ novel, patchNovel, user }) => {
-  const currentNovelDetails = {
-    title: novel.title,
-    summary: novel.summary,
-    plan: novel.plan,
-    goal: novel.goal
+class EditNovel extends Component {
+  state = {
+    title: this.props.novel.title,
+    summary: this.props.novel.summary,
+    plan: this.props.novel.plan,
+    goal: this.props.novel.goal
   }
-  return (
-    <div>
-      <p>Edit your novel:</p>
-      <NovelForm initialForm={currentNovelDetails} submitAction={patchNovel} user={user} novel={novel}/>
-      <Link to="/novel">Back</Link>
-    </div>
-  );
+
+  planTypes = () => {
+    return [
+      "November Warrior",
+      "Slow and Steady",
+      "Revise Revise",
+      "Hi, I'm in School",
+      "A Little of Everything",
+      "Condensed Timeline",
+      "My Way or the Highway"
+    ]
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    const novel = {
+      ...this.state
+    }
+    this.props.createNovel(novel, this.props.user)
+    // console.log(this.props.submitAction)
+    // console.log(this.props.user)
+    // console.log(novel)
+    // this.props.patchWordCount(updatedNovel)
+
+    // this.setState({
+    //   title: "",
+    //   summary: "",
+    //   plan: "",
+    //   goal: 0
+    // })
+  }
+  render() {
+    return (
+      <div>
+        <p>Edit your novel:</p>
+        <form onSubmit={this.handleSubmit}>
+            <label>
+              Title:
+              <input id="title" type="text" name="title" onChange={this.handleChange} value={this.state.title}/>
+            </label>
+            <label>
+              Summary:
+              <input id="summary" type="textarea" name="summary" onChange={this.handleChange} value={this.state.summary}/>
+            </label>
+            <label>
+              Plan:
+              <select id="plan" name="plan" onChange={this.handleChange} value={this.state.plan}>
+                <option value="">--Please choose a plan--</option>
+                {this.planTypes().map(planType => {
+                  //make new Option for each planType
+                  return <option key={planType} value={planType}>{planType}</option>
+                })}
+              </select>
+            </label>
+            <label>
+              Word count goal:
+              <input id="goal" type="number" min="1" name="goal" onChange={this.handleChange} value={this.state.goal}/>
+            </label>
+            <input type="submit"/>
+          </form>
+          <Link to="/novel">Back</Link>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
   return {
-    user: state.currentUser
+    user: state.currentUser,
+    novel: state.currentNovel
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    patchNovel: (novel, user) => dispatch(patchNovel(novel, user))
+    patchNovel: (novel) => dispatch(patchNovel(novel))
   }
 }
 
