827b43fde9f29bb76d4c0a091299061324fefbdf
Sam Markham <sam.e.markham@gmail.com>
2020-04-21 19:36:07 -0700

    Rejigs create novel component to have its own form instead of using NovelForm

--- src/components/CreateNovel.js
+++ src/components/CreateNovel.js
@@ -1,22 +1,121 @@
-import React from 'react';
-import NovelForm from './NovelForm';
+// import React from 'react';
+// import NovelForm from './NovelForm';
+// import { connect } from 'react-redux';
+// import { createNovel } from '../actions/novels';
+
+
+// const CreateNovel = (props) => {
+//   const emptyForm = {
+//     title: "",
+//     summary: "",
+//     plan: "",
+//     goal: ""
+//   }
+//   return (
+//     <div>
+//       <p>Create your novel: </p>
+//       <NovelForm initialForm={emptyForm} submitAction={props.createNovel} user={props.user}/>
+
+//     </div>
+//   );
+// }
+
+// const mapStateToProps = state => {
+//   return {
+//     user: state.currentUser
+//   }
+// }
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     createNovel: (novel, user) => dispatch(createNovel(novel, user))
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CreateNovel);
+
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createNovel } from '../actions/novels';
 
+class CreateNovel extends Component {
 
-const CreateNovel = (props) => {
-  const emptyForm = {
+  state = {
     title: "",
     summary: "",
     plan: "",
-    goal: ""
+    goal: 0
   }
-  return (
-    <div>
-      <p>Create your novel: </p>
-      <NovelForm initialForm={emptyForm} submitAction={props.createNovel} user={props.user}/>
-    </div>
-  );
+
+  planTypes = () => {
+    return [
+      "November Warrior",
+      "Slow and Steady",
+      "Revise Revise",
+      "Hi, I'm in School",
+      "A Little of Everything",
+      "Condensed Timeline",
+      "My Way or the Highway"
+    ]
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    const novel = {
+      ...this.state
+    }
+    this.props.createNovel(novel, this.props.user)
+    // console.log(this.props.submitAction)
+    // console.log(this.props.user)
+    // console.log(novel)
+    // this.props.patchWordCount(updatedNovel)
+
+    this.setState({
+      title: "",
+      summary: "",
+      plan: "",
+      goal: 0
+    })
+  }
+
+  render() {
+
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Title:
+            <input id="title" type="text" name="title" onChange={this.handleChange} value={this.state.title}/>
+          </label>
+          <label>
+            Summary:
+            <input id="summary" type="textarea" name="summary" onChange={this.handleChange} value={this.state.summary}/>
+          </label>
+          <label>
+            Plan:
+            <select id="plan" name="plan" onChange={this.handleChange} value={this.state.plan}>
+              <option value="">--Please choose a plan--</option>
+              {this.planTypes().map(planType => {
+                //make new Option for each planType
+                return <option key={planType} value={planType}>{planType}</option>
+              })}
+            </select>
+          </label>
+          <label>
+            Word count goal:
+            <input id="goal" type="number" min="1" name="goal" onChange={this.handleChange} value={this.state.goal}/>
+          </label>
+          <input type="submit"/>
+        </form>
+      );
+    }
 }
 
 const mapStateToProps = state => {
