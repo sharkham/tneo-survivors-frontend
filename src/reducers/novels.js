0c45061147497885379722c1a27aff8009038391
Sam Markham <sam.e.markham@gmail.com>
2020-04-21 18:46:59 -0700

    Adds case to novels reducer

--- src/reducers/novels.js
+++ src/reducers/novels.js
@@ -2,6 +2,8 @@ export default function novels(state = [], action) {
   switch (action.type) {
     case 'ADD_NOVEL':
       return [...state, action.novel]
+    case 'UPDATE_NOVEL':
+      return [...state, action.novel]
     default:
       return state
   }
