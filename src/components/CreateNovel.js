ab953b64cf7ab5acbe10cbd4656ea7f11afea1de
Sam Markham <sam.e.markham@gmail.com>
2020-04-21 19:45:06 -0700

    Removes unused code from CreateNovel

--- src/components/CreateNovel.js
+++ src/components/CreateNovel.js
@@ -1,39 +1,3 @@
-// import React from 'react';
-// import NovelForm from './NovelForm';
-// import { connect } from 'react-redux';
-// import { createNovel } from '../actions/novels';
-
-
-// const CreateNovel = (props) => {
-//   const emptyForm = {
-//     title: "",
-//     summary: "",
-//     plan: "",
-//     goal: ""
-//   }
-//   return (
-//     <div>
-//       <p>Create your novel: </p>
-//       <NovelForm initialForm={emptyForm} submitAction={props.createNovel} user={props.user}/>
-
-//     </div>
-//   );
-// }
-
-// const mapStateToProps = state => {
-//   return {
-//     user: state.currentUser
-//   }
-// }
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     createNovel: (novel, user) => dispatch(createNovel(novel, user))
-//   }
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(CreateNovel);
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createNovel } from '../actions/novels';
