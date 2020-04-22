1b69d027cd7169e710464b006336175c435561d9
Sam Markham <sam.e.markham@gmail.com>
2020-04-22 12:05:13 -0700

    Wires up NovelBadgesContainer to get badgetypes from store

--- src/components/NovelBadgesContainer.js
+++ src/components/NovelBadgesContainer.js
@@ -1,41 +1,25 @@
 import React, { Component } from 'react';
-import Badges from './Badges'
-import BadgesForm from './BadgesForm'
+import Badges from './Badges';
+import BadgesForm from './BadgesForm';
+import { connect } from 'react-redux';
 
 class NovelBadgesContainer extends Component {
   //this component is going to access store to get and set info
 
-  state = {
-    badgetypes: [
-      {
-        "id": 1,
-        "name": "Starting to get started",
-        "description": "updated spreadsheet",
-        "emoji": "🎉",
-        },
-        {
-        "id": 2,
-        "name": "Starry Night",
-        "description": "night scene",
-        "emoji": "🌉",
-        },
-        {
-        "id": 3,
-        "name": "Here comes the sun!",
-        "description": "morning scene",
-        "emoji": "🌅",
-        }
-    ]
-  }
-
   render() {
     return (
       <div>
-        <BadgesForm badgetypes={this.state.badgetypes}/>
+        <BadgesForm badgetypes={this.props.badgetypes}/>
         <Badges />
       </div>
     );
   }
 }
 
-export default NovelBadgesContainer;
+const mapStateToProps = state => {
+  return {
+    badgetypes: state.badgetypes
+  }
+}
+
+export default connect(mapStateToProps)(NovelBadgesContainer);
