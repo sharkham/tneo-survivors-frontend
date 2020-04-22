5dcab502029f81b9ed92ac8e12c831cb4f041627
Sam Markham <sam.e.markham@gmail.com>
2020-04-22 12:11:12 -0700

    Changes BadgesForm to a class component

--- src/components/BadgesForm.js
+++ src/components/BadgesForm.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BadgesForm = (props) => {
+class BadgesForm extends Component {
   //render the badges in the form from badgetype props passed down
-  return (
-    <form>
-      <select className="badgeselect">
-        <option value="">--Please choose a badge--</option>
-        {props.badgetypes.map(badgetype => {
-          //make new Option for each badgetype
-          return <option key={badgetype.id} value={badgetype.id}>{badgetype.emoji} - {badgetype.name} - {badgetype.description}</option>
-        })}
-      </select>
-    </form>
-  );
+  render() {
+    return (
+      <form>
+        <select className="badgeselect">
+          <option value="">--Please choose a badge--</option>
+          {this.props.badgetypes.map(badgetype => {
+            //make new Option for each badgetype
+            return <option key={badgetype.id} value={badgetype.id}>{badgetype.emoji} - {badgetype.name} - {badgetype.description}</option>
+          })}
+        </select>
+        <input type="submit"/>
+      </form>
+    );
+  }
 }
 
 export default BadgesForm;
