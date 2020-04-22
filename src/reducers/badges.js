ec23574f551ac1793ceb73fe3e1b60896de9b018
Sam Markham <sam.e.markham@gmail.com>
2020-04-22 12:47:45 -0700

    Fixes ADD_BADGE action in badges reducer

--- src/reducers/badges.js
+++ src/reducers/badges.js
@@ -1,9 +1,7 @@
 export default function badges(state = [], action) {
   switch (action.type) {
     case 'ADD_BADGE':
-      return {
-        ...state
-      }
+      return [...state, action.badge]
     default:
       return state
   }
