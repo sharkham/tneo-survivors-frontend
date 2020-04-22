74bb715b7d69a11cd73f6d43234bb8016477fd61
Sam Markham <sam.e.markham@gmail.com>
2020-04-22 12:28:20 -0700

    Adds handleChange and handleSubmit functionality to BadgesForm

--- src/components/BadgesForm.js
+++ src/components/BadgesForm.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 
 class BadgesForm extends Component {
   //render the badges in the form from badgetype props passed down
+  state = {
+    badgetypeId: ""
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: parseInt(value)
+    })
+    console.log(this.state)
+  }
+
+  getBadgetypeById = (id) => {
+    return this.props.badgetypes.find(badgetype => badgetype.id === id)
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const badgetype = this.getBadgetypeById(this.state.badgetypeId)
+    console.log(badgetype)
+  }
+
   render() {
     return (
-      <form>
-        <select className="badgeselect">
+      <form onSubmit={this.handleSubmit}>
+        <select onChange={this.handleChange} name="badgetypeId" className="badgeselect">
           <option value="">--Please choose a badge--</option>
           {this.props.badgetypes.map(badgetype => {
             //make new Option for each badgetype
