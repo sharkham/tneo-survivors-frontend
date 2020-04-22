1f8c54d7b70f981ffd39101044fab0decb40bb5e
Sam Markham <sam.e.markham@gmail.com>
2020-04-22 16:21:56 -0700

    Adds an AboutPage component

---
+++ src/components/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <p>This is a novel contest app! Users can make a new novel for every year they're participating and keep track of things like word count and badges earned.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
