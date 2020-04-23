6fd6bba9071bc39667ec6f6336764779c8b396e6
Sam Markham <sam.e.markham@gmail.com>
2020-04-23 16:18:49 -0700

    Adds rows and columns to AllNovelsIndex

--- src/components/AllNovelsIndex.js
+++ src/components/AllNovelsIndex.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import NovelCard from './NovelCard';
+import { Container, Col, Row } from 'reactstrap';
 
 class AllNovelsIndex extends Component {
   render() {
     return (
       <div>
         <p>All of this year's novels:</p>
-        {this.props.novels.map(novel => {
-          return <NovelCard key={novel.id} novel={novel} currentUser={this.props.currentUser}/>
-        })}
+        <Container>
+          <Row xs="3">
+
+            {this.props.novels.map(novel => {
+              return <Col><NovelCard key={novel.id} novel={novel} currentUser={this.props.currentUser}/></Col>
+            })}
+          </Row>
+        </Container>
       </div>
     );
   }
