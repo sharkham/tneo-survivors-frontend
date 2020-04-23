b435808a98a45ca84c3c24b64eacfb5822522b3c
Sam Markham <sam.e.markham@gmail.com>
2020-04-23 14:08:32 -0700

    Adds iteration over badges to return SmallBadge for each badge in SmallBadgesContainer

--- src/components/SmallBadgesContainer.js
+++ src/components/SmallBadgesContainer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import SmallBadge from './SmallBadge'
 
 const SmallBadgesContainer = (props) => {
   return (
     <div>
-
+      {/* go back later and restrict number of iterations */}
+      {props.badges.map(badge => {
+        return <SmallBadge key={badge.id} badge={badge} />
+      })}
     </div>
   );
 }
