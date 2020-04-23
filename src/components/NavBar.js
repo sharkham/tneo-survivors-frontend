8b08559da836cc0a643f2a621117213cc0ee1a39
Sam Markham <sam.e.markham@gmail.com>
2020-04-23 14:58:16 -0700

    Fixes formatting for Navbar components and adds links to about and novel pages in NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,10 @@ import { getCurrentUser } from '../actions/currentUser';
 import { Link } from 'react-router-dom';
 import {
   Navbar,
-  NavbarBrand
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
 } from 'reactstrap';
 
 
@@ -19,17 +22,20 @@ class NavBar extends Component {
   render() {
     return (
       <Navbar color="light" light expand="md">
-        <NavbarBrand><Link to="/">Novel Contest App</Link></NavbarBrand>
-        <ul>
-          { this.props.currentUser ? <li>Welcome, {this.props.currentUser.name}</li> : "" }
+        <NavbarBrand tag={Link} to="/">Novel Contest App</NavbarBrand>
+        <Nav className="mr-auto" navbar>
+          { this.props.currentUser ? <NavItem><NavLink tag={Link} to="/about">About</NavLink></NavItem> : "" }
+          { this.props.currentUser ? <NavItem><NavLink tag={Link} to="/novel">Novel</NavLink></NavItem> : "" }
+        </Nav>
+        {/* <ul> */}
+          {/* { this.props.currentUser ? <li>Welcome, {this.props.currentUser.name}</li> : "" } */}
           {/* <li><Link to="/">Home</Link></li> */}
-          { this.props.currentUser ? <li><Link to="/about">About</Link></li> : ""}
-          { this.props.currentUser ? <li><Link to="/novel">Novel</Link></li> : "" }
-          { this.props.currentUser ? <li><Link to="/previous_novels">Previous Novels </Link></li> : ""}
+          {/* { this.props.currentUser ? <li><Link to="/about">About</Link></li> : ""} */}
+          {/* { this.props.currentUser ? <li><Link to="/previous_novels">Previous Novels </Link></li> : ""} */}
           {/* Add previous novels page here later */}
           {/* <li><Link to="/profile">Profile</Link></li> */}
-          { this.props.currentUser ? <li><Logout /></li> : ""}
-        </ul>
+          {/* { this.props.currentUser ? <li><Logout /></li> : ""} */}
+        {/* </ul> */}
       </Navbar>
     );
   }
