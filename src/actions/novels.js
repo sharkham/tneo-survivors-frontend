fb0292480b5485e8d9dff9e343b7c3edf75b8208
Sam Markham <sam.e.markham@gmail.com>
2020-04-23 12:22:32 -0700

    Adds getNovels and associated methods to novels action file

--- src/actions/novels.js
+++ src/actions/novels.js
@@ -18,7 +18,29 @@ export const updateNovel = novel => {
   }
 }
 
+export const setCurrentNovel = novel => {
+  return {
+    type: "SET_CURRENT_NOVEL",
+    novel
+  }
+}
+
+export const setOtherUserNovels = novels => {
+  return {
+    type: "SET_OTHER_USER_NOVELS",
+    novels
+  }
+}
+
+export const setOtherNovels = novels => {
+  return {
+    type: "SET_OTHER_NOVELS",
+    novels
+  }
+}
+
 //asynchronous action creators
+
 export const createNovel = (novel, user) => {
   return dispatch => {
     return fetch(`${baseURL}/users/${user.id}/novels`, {
@@ -63,5 +85,56 @@ export const patchNovel = (novel) => {
       }
     })
     .catch(console.log)
+  }
+}
+
+
+export const getNovels = user => {
+  // console.log("getNovel")
+  return dispatch => {
+    return fetch(`${baseURL}/novels`, {
+      //grab the user, get the novel where the novel year matches current year.
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+      //body: JSON.stringify({username: "marysue1", password: "password"})
+    })
+    .then(res => res.json())
+    .then(novels => {
+      if (!!findCurrentNovel(novels, user)) {
+        dispatch(setCurrentNovel(findCurrentNovel(novels, user)))
+      }
+      if (!!findOtherUserNovels(novels, user)) {
+        dispatch(setOtherUserNovels(findOtherUserNovels(novels, user)))
+      }
+      if (!!findOtherCurrentNovels(novels, user)) {
+        dispatch(setOtherNovels(findOtherCurrentNovels(novels, user)))
+      }
+    })
+    .catch(console.log)
+  }
+}
+
+//helpers
+const findCurrentNovel = (novels, user) => {
+  const currentYear = new Date().getFullYear()
+  if (!!novels.find(novel => novel.year === currentYear && novel.user_id === user.id)) {
+    return novels.find(novel => novel.year === currentYear && novel.user_id === user.id)
+  }
+}
+
+const findOtherUserNovels = (novels, user) => {
+  const currentYear = new Date().getFullYear()
+  if (!!novels.find(novel => novel.year !== currentYear && novel.user_id === user.id)) {
+    return novels.filter(novel => novel.year !== currentYear && novel.user_id === user.id)
+  }
+}
+
+const findOtherCurrentNovels = (novels, user) => {
+  const currentYear = new Date().getFullYear()
+  if (!!novels.find(novel => novel.year === currentYear && novel.user_id !== user.id)) {
+    return novels.filter(novel => novel.year === currentYear && novel.user_id !== user.id)
   }
 }
