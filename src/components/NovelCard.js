0c65114e1386cd309d4a61dd60658404b106bb60
Sam Markham <sam.e.markham@gmail.com>
2020-04-23 16:43:10 -0700

    Gives NovelCard another 'container' prop so it will conditionally render more info if it's in use in PreviousNovelsPage

--- src/components/NovelCard.js
+++ src/components/NovelCard.js
@@ -2,16 +2,17 @@ import React from 'react';
 import SmallBadgesContainer from './SmallBadgesContainer';
 import { Card, CardHeader, CardText, CardTitle, CardBody } from 'reactstrap';
 
-const NovelCard = ({ novel, currentUser }) => {
+const NovelCard = ({ novel, currentUser, container }) => {
   return (
     <Card>
       <CardHeader>{novel.title}</CardHeader>
       <CardBody>
-        {novel.user.name !== currentUser.name ? <CardTitle>by {novel.user.name}</CardTitle> : ""}
-        <CardText>{novel.summary}</CardText>
-        {novel.user.name === currentUser.name ? <CardTitle>Plan: {novel.plan}</CardTitle> : ""}
-        {novel.user.name === currentUser.name ? <CardTitle>Goal: {novel.goal}</CardTitle> : ""}
-        {novel.user.name === currentUser.name ? <CardTitle>Final Word Count: {novel.wordcount}</CardTitle> : ""}
+        {container !== "PreviousNovelsPage" ? <CardTitle>by {novel.user.name}</CardTitle> : ""}
+        {/* <CardText>{novel.summary}</CardText> */}
+        {container === "PreviousNovelsPage" ? <CardTitle>Summary: {novel.summary}</CardTitle> : ""}
+        {container === "PreviousNovelsPage" ? <CardTitle>Plan: {novel.plan}</CardTitle> : ""}
+        {container === "PreviousNovelsPage" ? <CardTitle>Goal: {novel.goal}</CardTitle> : ""}
+        {container === "PreviousNovelsPage" ? <CardTitle>Final Word Count: {novel.wordcount}</CardTitle> : ""}
         <SmallBadgesContainer badges={novel.badges} />
       </CardBody>
     </Card>
--- src/components/PreviousNovelsPage.js
+++ src/components/PreviousNovelsPage.js
@@ -6,7 +6,7 @@ const PreviousNovelsPage = (props) => {
   return (
     <div>
       {/* {if (props.novels)} */}
-      {props.novels.length > 0 ? props.novels.map(novel => <NovelCard key={novel.id} novel={novel} currentUser={props.currentUser}/>) : <p>You don't have any previous novels yet!</p>}
+      {props.novels.length > 0 ? props.novels.map(novel => <NovelCard key={novel.id} novel={novel} currentUser={props.currentUser} container="PreviousNovelsPage"/>) : <p>You don't have any previous novels yet!</p>}
       {/* Previous novels go here! */}
     </div>
   );
