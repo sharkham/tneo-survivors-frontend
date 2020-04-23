9de8c975aaa017d11ae85808906600df3e6b330f
Sam Markham <sam.e.markham@gmail.com>
2020-04-23 12:51:34 -0700

    Adds CLEAR_CURRENT_USER case to otherUserNovels to clear that array when user logs out

--- src/reducers/otherUserNovels.js
+++ src/reducers/otherUserNovels.js
@@ -2,6 +2,8 @@ export default function otherUserNovels(state = [], action) {
   switch (action.type) {
     case 'SET_OTHER_USER_NOVELS':
       return action.novels
+    case 'CLEAR_CURRENT_USER':
+      return []
     default:
       return state
   }
