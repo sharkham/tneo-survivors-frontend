81e9052b88b1e3d706072a5828626896fb8653b5
Sam Markham <sam.e.markham@gmail.com>
2020-04-23 16:24:38 -0700

    Adds conditional rendering for additional info if NovelCard is for one of user's novels

--- src/components/NovelCard.js
+++ src/components/NovelCard.js
@@ -9,6 +9,9 @@ const NovelCard = ({ novel, currentUser }) => {
       <CardBody>
         {novel.user.name !== currentUser.name ? <CardTitle>by {novel.user.name}</CardTitle> : ""}
         <CardText>{novel.summary}</CardText>
+        {novel.user.name === currentUser.name ? <CardTitle>Plan: {novel.plan}</CardTitle> : ""}
+        {novel.user.name === currentUser.name ? <CardTitle>Goal: {novel.goal}</CardTitle> : ""}
+        {novel.user.name === currentUser.name ? <CardTitle>Final Word Count: {novel.wordcount}</CardTitle> : ""}
         <SmallBadgesContainer badges={novel.badges} />
       </CardBody>
     </Card>
