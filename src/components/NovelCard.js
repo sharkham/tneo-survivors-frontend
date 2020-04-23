56af5f7a9d1b6a2ed363577bd4a0b1def40d7bb5
Sam Markham <sam.e.markham@gmail.com>
2020-04-23 14:05:42 -0700

    Adds SmallBadgesContainer to NovelCard

--- src/components/NovelCard.js
+++ src/components/NovelCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SmallBadgesContainer from './SmallBadgesContainer'
 
 const NovelCard = ({ novel, currentUser }) => {
   return (
@@ -6,6 +7,7 @@ const NovelCard = ({ novel, currentUser }) => {
       <p>{novel.title}</p>
       {novel.user.name !== currentUser.name ? <p>by {novel.user.name}</p> : ""}
       <p>{novel.summary}</p>
+      <SmallBadgesContainer badges={novel.badges} />
     </div>
   );
 }
