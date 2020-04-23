50eb349dfca697ffc83212f4fe3a796aad0df519
Sam Markham <sam.e.markham@gmail.com>
2020-04-23 14:38:42 -0700

    Imports bootstrap in index

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
