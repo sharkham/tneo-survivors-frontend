d91be5e4d41c0e754198bf9a771b3b1af76ffbc2
Sam Markham <sam.e.markham@gmail.com>
2020-04-24 13:44:39 -0700

    Removes unused badges reducer

--- src/reducers/badges.js
+++
@@ -1,9 +0,0 @@
-export default function badges(state = [], action) {
-  switch (action.type) {
-    case 'ADD_BADGE':
-      return [...state, action.badge]
-    default:
-      return state
-  }
-
-}
