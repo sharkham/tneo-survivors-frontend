d4df241b96926363730d6365421bf5f8532004a3
Sam Markham <sam.e.markham@gmail.com>
2020-04-24 14:46:29 -0700

    Changes button text on Badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -4,7 +4,7 @@ import { Button } from 'reactstrap';
 const Badge = ({ badge, destroyBadge }) => {
   return (
     <div>
-      {badge.emoji} - {badge.name} - {badge.description} <Button size="sm" color="secondary" onClick={() => destroyBadge(badge.id)}>x</Button>
+      {badge.emoji} - {badge.name} - {badge.description} <Button size="sm" color="secondary" onClick={() => destroyBadge(badge.id)}> X </Button>
     </div>
   );
 }
