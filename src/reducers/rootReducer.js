5161198c84d9c8f41a70c5e6195ebf5a3fecf5df
Sam Markham <sam.e.markham@gmail.com>
2020-04-24 13:44:24 -0700

    Cleans up rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,6 @@
-// import manageRestaurants from './manageRestaurant'
-// import manageReviews from './manageReview'
 import { combineReducers } from 'redux'
 import users from './users'
 import allCurrentNovels from './allCurrentNovels'
-// import badges from './badges'
 import badgetypes from './badgetypes'
 import currentUser from './currentUser'
 import currentNovel from './currentNovel'
@@ -15,10 +12,7 @@ const rootReducer = combineReducers({
   allCurrentNovels,
   currentNovel,
   otherUserNovels,
-  // badges,
   badgetypes
-  // restaurants: manageRestaurants,
-  // reviews: manageReviews
 })
 
 export default rootReducer;
