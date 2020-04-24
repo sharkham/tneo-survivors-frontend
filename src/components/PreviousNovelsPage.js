29a1eeee8832a58e218eb81f32d212fcf0188bcf
Sam Markham <sam.e.markham@gmail.com>
2020-04-24 12:22:14 -0700

    Adds CardColumns wrapper to PreviousNovelsPage

--- src/components/PreviousNovelsPage.js
+++ src/components/PreviousNovelsPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import NovelCard from './NovelCard';
+import { CardColumns } from 'reactstrap';
 
 const PreviousNovelsPage = (props) => {
   return (
-    <div>
+    <CardColumns>
       {/* {if (props.novels)} */}
       {props.novels.length > 0 ? props.novels.map(novel => <NovelCard key={novel.id} novel={novel} currentUser={props.currentUser} container="PreviousNovelsPage"/>) : <p>You don't have any previous novels yet!</p>}
       {/* Previous novels go here! */}
-    </div>
+    </CardColumns>
   );
 }
 
