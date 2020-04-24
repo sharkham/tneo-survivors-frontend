c5bf078c55b6d5ce155fbfc7830adf1598fa7967
Sam Markham <sam.e.markham@gmail.com>
2020-04-24 13:00:20 -0700

    Adds ADD_BADGE case to allCurrentNovels reducer

--- src/reducers/allCurrentNovels.js
+++ src/reducers/allCurrentNovels.js
@@ -16,6 +16,17 @@ export default function allCurrentNovels(state = [], action) {
           return novel;
         }
       })
+    case 'ADD_BADGE':
+      return state.map(novel => {
+        if (novel.id === action.badge.novel_id) {
+          return {
+            ...novel,
+            badges: [...novel.badges, action.badge]
+          };
+        } else {
+          return novel;
+        }
+      })
     case 'SET_OTHER_NOVELS':
       return [...state, ...action.novels]
     case 'CLEAR_CURRENT_USER':
