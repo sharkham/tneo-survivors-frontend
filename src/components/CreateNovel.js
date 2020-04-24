e0ef46235aacbb9cb2bca7b5b7d66c10c00beeab
Sam Markham <sam.e.markham@gmail.com>
2020-04-24 13:51:50 -0700

    Adds card styling to CreateNovel

--- src/components/CreateNovel.js
+++ src/components/CreateNovel.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createNovel } from '../actions/novels';
-import { Col, Button, Form, FormGroup, Input, Label } from 'reactstrap';
+import { Col,
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Card,
+  CardHeader,
+  CardBody } from 'reactstrap';
 
 class CreateNovel extends Component {
 
@@ -54,42 +62,36 @@ class CreateNovel extends Component {
 
   render() {
     return (
-      <div>
-        <h4>Create your novel:</h4>
-        <Form onSubmit={this.handleSubmit}>
-          <FormGroup row>
-            <Label for="title" sm={2}>Title:</Label>
-            <Col sm={10}>
-              <Input type="text" name="title" id="title" placeholder="novel title" onChange={this.handleChange} value={this.state.title}/>
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label for="summary" sm={2}>Summary:</Label>
-            <Col sm={10}>
-              <Input type="textarea" name="summary" id="summary" onChange={this.handleChange} value={this.state.summary}/>
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label for="plan" sm={2}>Plan of Attack:</Label>
-            <Col sm={10}>
-              <Input type="select" id="plan" name="plan" onChange={this.handleChange} value={this.state.plan}>
-                <option value="">--Please choose a plan--</option>
-                {this.planTypes().map(planType => {
-                  //make new Option for each planType
-                  return <option key={planType} value={planType}>{planType}</option>
-                })}
-              </Input>
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label for="goal" sm={2}>Word Count Goal:</Label>
-            <Col sm={10}>
-              <Input type="number" min="1" name="goal" id="goal" onChange={this.handleChange} value={this.state.goal}/>
-            </Col>
-          </FormGroup>
-          <Button color="secondary">Submit</Button>
-        </Form>
-      </div>
+      <Card>
+        <CardHeader>Create your novel:</CardHeader>
+        <CardBody>
+          <Form onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Label for="title">Title:</Label>
+                <Input type="text" name="title" id="title" placeholder="novel title" onChange={this.handleChange} value={this.state.title}/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="summary">Summary:</Label>
+                <Input type="textarea" name="summary" id="summary" onChange={this.handleChange} value={this.state.summary}/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="plan">Plan of Attack:</Label>
+                <Input type="select" id="plan" name="plan" onChange={this.handleChange} value={this.state.plan}>
+                  <option value="">--Please choose a plan--</option>
+                  {this.planTypes().map(planType => {
+                    //make new Option for each planType
+                    return <option key={planType} value={planType}>{planType}</option>
+                  })}
+                </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="goal">Word Count Goal:</Label>
+                <Input type="number" min="1" name="goal" id="goal" onChange={this.handleChange} value={this.state.goal}/>
+            </FormGroup>
+            <Button color="secondary">Submit</Button>
+          </Form>
+        </CardBody>
+      </Card>
     );
   }
 }
