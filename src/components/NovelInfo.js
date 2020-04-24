7da82de1d9b3f232cd4362838942961e91809a34
Sam Markham <sam.e.markham@gmail.com>
2020-04-24 12:29:16 -0700

    Moves BasicInfoDisplay component content into NovelInfo

--- src/components/NovelInfo.js
+++ src/components/NovelInfo.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import UpdateWordCountForm from './UpdateWordCountForm';
-import BasicInfoDisplay from './BasicInfoDisplay';
 import CreateNovel from './CreateNovel'
 import { connect } from 'react-redux';
 import { patchWordCount } from '../actions/novels';
 import { Link } from 'react-router-dom';
-import { Card } from 'reactstrap';
+import { Card, CardHeader, CardBody, CardText } from 'reactstrap';
 
 
 
@@ -16,9 +15,19 @@ class NovelInfo extends Component {
     return (
       <div>
         <Card>
-          <BasicInfoDisplay novel={this.props.novel}/>
-          <Link to="/novel/edit">Edit Novel</Link>
-          <UpdateWordCountForm novel={this.props.novel} wordcount={this.props.novel.wordcount} patchWordCount={this.props.patchWordCount}/>
+          <CardHeader>Title: {this.props.novel.title}</CardHeader>
+          <CardBody>
+            <CardText>Summary: {this.props.novel.summary}</CardText>
+            <CardText>Plan of Attack: {this.props.novel.plan}</CardText>
+            <CardText>Word Count Goal: {this.props.novel.goal}</CardText>
+            <CardText>Current Word Count: {this.props.novel.wordcount}</CardText>
+            <CardText><Link to="/novel/edit">Edit Novel</Link></CardText>
+            <UpdateWordCountForm
+              novel={this.props.novel}
+              wordcount={this.props.novel.wordcount}
+              patchWordCount={this.props.patchWordCount}
+            />
+          </CardBody>
         </Card>
       </div>
     );
