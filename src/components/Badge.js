8a9233a7d27e982f184f9ff2ed8d925ffbee1c39
Sam Markham <sam.e.markham@gmail.com>
2020-04-24 20:19:52 -0700

    Increases badge size in Badge component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -4,7 +4,7 @@ import { Button } from 'reactstrap';
 const Badge = ({ badge, destroyBadge }) => {
   return (
     <div>
-      {badge.emoji} - {badge.name} - {badge.description} <Button size="sm" color="secondary" onClick={() => destroyBadge(badge.id)}> X </Button>
+      <span style={{fontSize: 24}}>{badge.emoji}</span> - {badge.name} - {badge.description} <Button size="sm" color="secondary" onClick={() => destroyBadge(badge.id)}> X </Button>
     </div>
   );
 }
