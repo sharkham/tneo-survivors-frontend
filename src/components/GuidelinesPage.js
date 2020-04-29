b520e4fba10cb1619e5a1d0a7cbbc61065c6ddf2
Sam Markham <sam.e.markham@gmail.com>
2020-04-29 15:05:13 -0700

    Adds GuidelinesPage component

---
+++ src/components/GuidelinesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GuidelinesPage = () => {
+  return (
+    <div>
+      <p>Specific contest guidelines live here.</p>
+    </div>
+  );
+}
+
+export default GuidelinesPage;
