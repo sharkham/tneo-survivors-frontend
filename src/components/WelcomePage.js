704cb62e5aaaf23674acac05a959e31062f6efef
Sam Markham <sam.e.markham@gmail.com>
2020-04-29 15:00:43 -0700

    Adds more info to WelcomePage

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -6,7 +6,9 @@ import { Link } from 'react-router-dom';
 const WelcomePage = () => {
   return (
     <div>
-      <p>Welcome, please <Link to="/login">log in</Link> or <Link to="/signup">sign up</Link>.</p>
+      <h4>Welcome!</h4>
+      <p>This is a novel contest app! Users can make a new novel for every year they're participating and keep track of things like word count and badges earned.</p>
+      <p>Please <Link to="/login">log in</Link> or <Link to="/signup">sign up</Link> to continue.</p>
     </div>
   );
 }
