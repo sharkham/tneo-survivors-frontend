193375e9b284caf65dc83771407462f220aacc62
Sam Markham <sam.e.markham@gmail.com>
2020-04-30 15:51:31 -0700

    Builds helper function file for resetPassword fetch

---
+++ src/helpers/resetPassword.js
@@ -0,0 +1,40 @@
+const baseURL = "http://localhost:3000/api/v1"
+
+export const resetPassword = (email) => {
+  return fetch(`${baseURL}/reset_password`, {
+    credentials: "include",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(email)
+  })
+  .then(res => res.json())
+  .then(res => {
+    console.log(res)
+  })
+  .catch(console.log)
+}
+
+// createPokemon(trainerId, pokedexEntry, boxNumber) {
+//   const pokemon = {
+//     pokedexid: pokedexEntry.id,
+//     name: pokedexEntry.name,
+//     number: boxNumber,
+//     image: pokedexEntry.image,
+//     icon: pokedexEntry.icon,
+//     height: pokedexEntry.height,
+//     position: "static",
+//     zindex: 0,
+//     trainer_id: trainerId
+//   }
+//   return fetch(this.baseUrl, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify({ pokemon: pokemon })
+//   }).then(res => res.json())
+//   .catch(error => console.error(error))
+// }
