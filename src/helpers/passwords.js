e0325bb7e7573eea37484d3c0b95443a507d82d9
Sam Markham <sam.e.markham@gmail.com>
2020-05-01 17:17:41 -0700

    Adds basic resetPassword method to passwords helper

--- src/helpers/passwords.js
+++ src/helpers/passwords.js
@@ -20,25 +20,22 @@ export const forgotPassword = (email) => {
   .catch(console.log)
 }
 
-// createPokemon(trainerId, pokedexEntry, boxNumber) {
-//   const pokemon = {
-//     pokedexid: pokedexEntry.id,
-//     name: pokedexEntry.name,
-//     number: boxNumber,
-//     image: pokedexEntry.image,
-//     icon: pokedexEntry.icon,
-//     height: pokedexEntry.height,
-//     position: "static",
-//     zindex: 0,
-//     trainer_id: trainerId
-//   }
-//   return fetch(this.baseUrl, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Accept": "application/json"
-//     },
-//     body: JSON.stringify({ pokemon: pokemon })
-//   }).then(res => res.json())
-//   .catch(error => console.error(error))
-// }
+export const resetPassword = (passwordObj) => {
+  console.log("resetPassword firing!")
+  return fetch(`${baseURL}/reset_password`, {
+    credentials: "include",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(passwordObj)
+  })
+  .then(res => res.json())
+  .then(response => {
+    alert(response.alert)
+  })
+  // .then(() => {
+  //   console.log("do the redirect here? Or just nothing?")
+  // })
+  .catch(console.log)
+}
