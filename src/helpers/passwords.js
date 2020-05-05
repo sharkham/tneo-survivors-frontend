ea1833f2cad942dea3ff38c5560c544627640935
Sam Markham <sam.e.markham@gmail.com>
2020-05-05 13:26:05 -0700

    Adds redux dispatch syntax to resetPassword method to try to get it to fire getCurrentUser after password is reset

--- src/helpers/passwords.js
+++ src/helpers/passwords.js
@@ -1,4 +1,4 @@
-import { login } from '../actions/currentUser'
+import { getCurrentUser } from '../actions/currentUser'
 
 const baseURL = "http://localhost:3000/api/v1"
 
@@ -24,27 +24,24 @@ export const forgotPassword = (email) => {
 
 export const resetPassword = (passwordObj) => {
   console.log("resetPassword firing!")
-  return fetch(`${baseURL}/reset_password`, {
-    credentials: "include",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(passwordObj)
-  })
-  .then(res => res.json())
-  .then(user => {
-    if (!!user.error) {
-      alert(user.error)
-    } else {
-      // console.log(user)
-      //possibly this needs to be done as a dispatch!
-      login({email: user.email, password: user.password})
-    }
-    // alert(response.error)
-  })
-  // .then(() => {
-  //   console.log("do the redirect here? Or just nothing?")
-  // })
-  .catch(console.log)
+  return dispatch => {
+    return fetch(`${baseURL}/reset_password`, {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(passwordObj)
+    })
+    .then(res => res.json())
+    .then(user => {
+      if (!!user.error) {
+        alert(user.error)
+      } else {
+        //possibly this needs to be done as a dispatch!
+        dispatch(getCurrentUser())
+      }
+    })
+    .catch(console.log)
+  }
 }
