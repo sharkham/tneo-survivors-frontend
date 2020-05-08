163fa08425ce351d0920c59d6f3dbc82d9e23474
Sam Markham <sam.e.markham@gmail.com>
2020-05-07 18:17:00 -0700

    Removes compose from index.js as it's not being used

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+// import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
 import 'bootstrap/dist/css/bootstrap.min.css';
