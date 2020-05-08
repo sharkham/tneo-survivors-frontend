ec519fade77d2976eb79c39e30857811bed8d87f
Sam Markham <sam.e.markham@gmail.com>
2020-05-07 21:21:03 -0700

    Adds link to Github in about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,8 +4,9 @@ const AboutPage = () => {
   return (
     <div>
       <p>This is a novel tracking app for novel contests! </p>
-      <p>It was designed for a novel contest that runs every year, throughout the year. With this app, users can create a record of their novel for the year and use the app to keep track of things like word count progress and badges for writing accomplishments, such as writing 1,000 words in one session. The plan of attack feature is specific to the novel contest this app was designed for, and reers to the user's strategy for novel writing, whether they're going to try to write it all in November, or little by little throughout the year, et cetera.</p>
+      <p>It was designed for a novel contest that runs every year, throughout the year. With this app, users can create a record of their novel for the year and use the app to keep track of things like word count progress and badges for writing accomplishments, such as writing 1,000 words in one session. The plan of attack feature is specific to the novel contest this app was designed for, and refers to the user's strategy for novel writing, whether they're going to try to write it all in November, or little by little throughout the year, et cetera.</p>
       <p>The app's home page displays all of the year's novels, so users can see how others participating in the contest are doing, and each new year, when users sign on to the app, they are prompted to create a new novel for that year's contest. Their previous novels will then be visible on the 'Previous Novels' page.</p>
+      <p>You can find this app's Github repo <a target="_blank" href="https://github.com/sharkham/novel-tracker-frontend">here</a>.</p>
     </div>
   );
 }
