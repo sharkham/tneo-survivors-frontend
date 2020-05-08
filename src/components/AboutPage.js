6414b0b4a6d1a8a307dcbb2404e411e7d91099c7
Sam Markham <sam.e.markham@gmail.com>
2020-05-07 21:16:25 -0700

    Adds more info to About Page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -3,7 +3,9 @@ import React from 'react';
 const AboutPage = () => {
   return (
     <div>
-      <p>This is a novel contest app! Users can make a new novel for every year they're participating and keep track of things like word count and badges earned.</p>
+      <p>This is a novel tracking app for novel contests! </p>
+      <p>It was designed for a novel contest that runs every year, throughout the year. With this app, users can create a record of their novel for the year and use the app to keep track of things like word count progress and badges for writing accomplishments, such as writing 1,000 words in one session.</p>
+      <p>The app's home page displays all of the year's novels, so users can see how others participating in the contest are doing, and each new year, when users sign on to the app, they are prompted to create a new novel for that year's contest. Their previous novels will then be visible on the 'Previous Novels' page.</p>
     </div>
   );
 }
