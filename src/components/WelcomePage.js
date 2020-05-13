209ad6f791c2d41422b382eee76bc5544a068fa6
Sam Markham <sam.e.markham@gmail.com>
2020-05-13 12:13:36 -0700

    Adds text to Welcome Page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -6,9 +6,10 @@ import { Link } from 'react-router-dom';
 const WelcomePage = () => {
   return (
     <div>
-      <h4>Welcome!</h4>
-      <p>This is a novel contest app! Users can make a new novel for every year they're participating and keep track of things like word count and badges earned.</p>
+      <h1>Novel Tracker App</h1>
+      <p>This is a novel tracker app for running novel contests! Users can make a new novel for every year they're participating and keep track of things like word count and badges earned.</p>
       <p>Please <Link to="/login">log in</Link> or <Link to="/signup">sign up</Link> to continue.</p>
+      <p>(Please note part of this app is hosted via a free server that sleeps when not in use. It may take a few extra seconds to wake up when you first access the site.)</p>
     </div>
   );
 }
