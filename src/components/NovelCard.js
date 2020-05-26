25839aeea76cc3047da7c32a7625e804c0da0003
Sam Markham <sam.e.markham@gmail.com>
2020-05-26 16:56:24 -0700

    Adds popover summary to NovelCard to render for AllNovelsIndex component (not PreviousNovels component).

--- src/components/NovelCard.js
+++ src/components/NovelCard.js
@@ -1,29 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SmallBadgesContainer from './SmallBadgesContainer';
-import { Card, CardHeader, CardText, CardTitle, CardBody, Progress } from 'reactstrap';
+import { Card, CardHeader, CardText, CardTitle, CardBody, Progress, Popover, PopoverBody, Button } from 'reactstrap';
 import { progressbar } from '../helpers/progressbar'
+import uuid from "uuid";
 
-const NovelCard = ({ novel, currentUser, container }) => {
+class NovelCard extends Component {
 
-  return (
-    <Card>
-      <CardHeader>
-        {novel.title}
-        {container === "PreviousNovelsPage" ? <> ({novel.year})</> : ""}
-        <Progress value={progressbar(novel.wordcount, novel.goal)}/>
-        <small className="text-muted">({novel.wordcount} / {novel.goal})</small>
-      </CardHeader>
-      <CardBody>
-        {container !== "PreviousNovelsPage" ? <CardTitle>by {novel.user.name}</CardTitle> : ""}
-        {/* <CardText>{novel.summary}</CardText> */}
-        {container === "PreviousNovelsPage" ? <CardText>Summary: {novel.summary}</CardText> : ""}
-        {/* <CardText>Plan of Attack: {novel.plan}</CardText> */}
-        {container === "PreviousNovelsPage" ? <CardText>Goal: {novel.goal}</CardText> : ""}
-        {container === "PreviousNovelsPage" ? <CardText>Final Word Count: {novel.wordcount}</CardText> : ""}
-        <SmallBadgesContainer badges={novel.badges} />
-      </CardBody>
-    </Card>
-  );
+  state = {
+    isOpen: false
+  }
+
+  toggle = () => {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen
+    }));
+  }
+
+  buttonId = () => {
+    let id = 0
+    if (id === 0) {
+      id = uuid()
+      return id
+    } else {
+      return id
+    }
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardHeader>
+          {this.props.novel.title}
+          {this.props.container === "PreviousNovelsPage" ? <> ({this.props.novel.year})</> : ""}
+          <Progress value={progressbar(this.props.novel.wordcount, this.props.novel.goal)}/>
+          <small className="text-muted">({this.props.novel.wordcount} / {this.props.novel.goal})</small>
+        </CardHeader>
+        <CardBody>
+          {this.props.container !== "PreviousNovelsPage" ? <CardTitle>by {this.props.novel.user.name}</CardTitle> : ""}
+          {/* <CardText>{novel.summary}</CardText> */}
+          {this.props.container === "PreviousNovelsPage" ? <CardText>Summary: {this.props.novel.summary}</CardText> : ""}
+          <CardText>Plan of Attack: {this.props.novel.plan}</CardText>
+          {this.props.container === "PreviousNovelsPage" ? <CardText>Goal: {this.props.novel.goal}</CardText> : ""}
+          {this.props.container === "PreviousNovelsPage" ? <CardText>Final Word Count: {this.props.novel.wordcount}</CardText> : ""}
+          <SmallBadgesContainer badges={this.props.novel.badges} />
+          {this.props.container !== "PreviousNovelsPage" ?
+          <div>
+            <Button id={"button-" + this.props.novel.id} color="secondary" style={{ marginBottom: '1rem' }} type="button">Summary</Button>
+            <Popover placement="bottom" isOpen={this.state.isOpen} target={"button-" + this.props.novel.id} toggle={this.toggle}>
+              <PopoverBody>{this.props.novel.summary}</PopoverBody>
+            </Popover>
+          </div>
+          : ""}
+        </CardBody>
+      </Card>
+    );
+  }
+
 }
+
+// = ({ novel, currentUser, container }) => {
+
+// }
 
 export default NovelCard;
